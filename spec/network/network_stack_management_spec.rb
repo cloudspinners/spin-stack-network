7a16a1ed1512e70fff13be8d0f9297c880d0eeb1
kief <kmorris@thoughtworks.com>
2018-08-20 16:03:28 +0100

    Pass in parameters needed for terraform variables

--- spec/network/network_stack_management_spec.rb
+++ spec/network/network_stack_management_spec.rb
@@ -5,14 +5,32 @@ RSpec.describe 'network stack instance management' do
     Cloudspin::Stack::Definition.from_file(terraform_source_folder + '/stack.yaml')
   }
 
+  let(:instance_parameter_values) {
+    {
+      'deployment_identifier' => 'my_env',
+      'component' => 'my_component',
+      'estate' => 'my_estate',
+      'base_dns_domain' => 'my_domain'
+    }
+  }
+
+  let(:required_resource_values) {
+    {
+      'assume_role_arn' => ''
+    }
+  }
+
   let(:stack_instance) {
     Cloudspin::Stack::Instance.new(
       stack_definition: stack_definition,
       backend_config: {},
       working_folder: working_folder,
-      statefile_folder: statefile_folder
+      statefile_folder: statefile_folder,
+      instance_parameter_values: instance_parameter_values,
+      required_resource_values: required_resource_values
     )
   }
+
 
   it 'is planned without error' do
     expect { stack_instance.plan }.not_to raise_error
